60711e279aeaca5f4993302b6cb125cf30a0ad66
Delaroy Studios <bamoyk@yahoo.com>
2018-02-14 20:09:45 +0100

    Initial commit

---
+++ app/src/main/java/com/delaroystudios/backup/LoginActivity.java
@@ -0,0 +1,241 @@
+// Copyright 2017 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.delaroystudios.backup;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.delaroystudios.backup.R;
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    /**
+     * A dummy authentication store containing known user names and passwords.
+     */
+    private static final String[] DUMMY_CREDENTIALS = new String[]{
+            "bamoyk@yahoo.com:hello", "info@delaroystudios.com:world"
+    };
+
+    /**
+     * Keep track of the login task to ensure we can cancel it if requested.
+     */
+    private UserLoginTask mAuthTask = null;
+
+    // UI references.
+    private AutoCompleteTextView mEmailView;
+    private EditText mPasswordView;
+    private View mProgressView;
+    private View mLoginFormView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        // Set up the login form.
+        mEmailView = findViewById(R.id.email);
+
+        mPasswordView = findViewById(R.id.password);
+        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == EditorInfo.IME_ACTION_DONE || id == EditorInfo.IME_NULL) {
+                    attemptLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        Button mEmailSignInButton = findViewById(R.id.email_sign_in_button);
+        mEmailSignInButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptLogin();
+            }
+        });
+        mLoginFormView = findViewById(R.id.login_form);
+        mProgressView = findViewById(R.id.login_progress);
+
+
+        if (!PrefUtils.needsLogin(this)) {
+            startActivity(new Intent(this, LoggedInActivity.class));
+            finish();
+        }
+
+        overlayDebug((TextView) findViewById(R.id.overlay));
+
+        //******************* Implement Login Hinting *****************
+        String accountHint = PrefUtils.getLoginHint(this);
+        if (accountHint != null) {
+            mEmailView.setHint(accountHint);
+        }
+        //******************* End implement Login Hinting *************
+    }
+
+    private void overlayDebug(@Nullable TextView overlay) {
+        if (overlay == null) {
+            return;
+        }
+        overlay.setText(PrefUtils.getDebugText(this));
+    }
+
+
+    /**
+     * Attempts to sign in or register the account specified by the login form.
+     * If there are form errors (invalid email, missing fields, etc.), the
+     * errors are presented and no actual login attempt is made.
+     */
+    private void attemptLogin() {
+        if (mAuthTask != null) {
+            return;
+        }
+
+        // Reset errors.
+        mEmailView.setError(null);
+        mPasswordView.setError(null);
+
+        // Store values at the time of the login attempt.
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        // Show a progress spinner, and kick off a background task to
+        // perform the user login attempt.
+        showProgress(true);
+        mAuthTask = new UserLoginTask(email, password);
+        mAuthTask.execute((Void) null);
+    }
+
+    /**
+     * Shows the progress UI and hides the login form.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+    }
+
+    /**
+     * Represents an asynchronous login/registration task used to authenticate
+     * the user.
+     */
+    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
+
+        private final String mEmail;
+        private final String mPassword;
+
+        UserLoginTask(String email, String password) {
+            mEmail = email;
+            mPassword = password;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            // TODO: attempt authentication against a network service.
+
+            try {
+                // Simulate network access.
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                return false;
+            }
+
+            for (String credential : DUMMY_CREDENTIALS) {
+                String[] pieces = credential.split(":");
+                if (pieces[0].equals(mEmail)) {
+                    // Account exists, return true if the password matches.
+                    if (pieces[1].equals(mPassword)) {
+                        // TODO: register the new account here and get back an authKey
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            mAuthTask = null;
+            showProgress(false);
+
+            if (success) {
+                finishLogin(mEmail, mPassword);
+            } else {
+                mPasswordView.setError(getString(R.string.error_incorrect_password));
+                mPasswordView.requestFocus();
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            mAuthTask = null;
+            showProgress(false);
+        }
+    }
+
+    private void finishLogin(String email, String password) {
+        // For the sake of this example, we assume that the authKey is the password
+        // Note: You should always ensure that usernames/passwords should be excluded
+        // from backup for security.
+        PrefUtils.setLoginAccount(LoginActivity.this, email, password);
+        startActivity(new Intent(this, LoggedInActivity.class));
+        finish();
+    }
+}
+
---
+++ app/src/main/java/com/delaroystudios/backup/PrefUtils.java
@@ -0,0 +1,146 @@
+// Copyright 2017 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.delaroystudios.backup;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.text.Html;
+import android.text.Spanned;
+
+/**
+ * Class for storing and accessing SharedPreferences
+ */
+class PrefUtils {
+
+    /**
+     * We store the login details such as the actual email address
+     * used for logging into our app in this preferences file.
+     *
+     * Since this has the actual details, we exclude it from backup.
+     */
+    private static final String PREF_LOGIN_DETAILS =
+            "com.delaroystudios.backup.PREF_LOGIN_DETAILS";
+
+    /**
+     * This shared preferences file contains information that should be backed up.
+     * Rather than actual email address, it contains a hints and other data that can
+     * be helpful for end users after a backup
+     */
+    private static final String PREF_APP_INFO =
+            "com.delaroystudios.backup.PREF_APP_INFO";
+
+    private static final String ACCOUNT_NAME = "ACCOUNT_NAME";
+    private static final String ACCOUNT_AUTH_KEY = "ACCOUNT_AUTH_KEY";
+    private static final String ACCOUNT_NAME_HINT = "ACCOUNT_NAME_HINT";
+
+    /* Class known for its static methods*/
+    private PrefUtils() {
+    }
+
+    /**
+     * Whether the App should show the login screen
+     * We store this in a shared preference that is <string>excluded<strong/> from
+     * backup.
+     *
+     * @param context Context for fetching SharedPreferences
+     * @return boolean indicating whether we should show the login screen
+     */
+    static boolean needsLogin(Context context) {
+        SharedPreferences preferences =
+                context.getSharedPreferences(PREF_LOGIN_DETAILS, Context.MODE_PRIVATE);
+        return preferences.getString(ACCOUNT_AUTH_KEY, null) == null ||
+                preferences.getString(ACCOUNT_NAME, null) == null;
+    }
+
+    /**
+     * Set the Login Account name once the login is successful.
+     *
+     * Here we also set an account name hint for the user to remember which account they
+     * used once the app has been restored.
+     *
+     * @param context Context used for SharedPreferences
+     * @param accountName The username/acountName to be set
+     * @param authKey The authKey to be used for login
+     */
+    static void setLoginAccount(@NonNull Context context, String accountName, String authKey) {
+        context.getSharedPreferences(PREF_LOGIN_DETAILS, Context.MODE_PRIVATE)
+                .edit()
+                .putString(ACCOUNT_NAME, accountName)
+                .putString(ACCOUNT_AUTH_KEY, authKey)
+                .apply();
+        context.getSharedPreferences(PREF_APP_INFO, Context.MODE_PRIVATE)
+                .edit()
+                .putString(ACCOUNT_NAME_HINT, createHint(accountName))
+                .apply();
+    }
+
+    static void logout(@NonNull Context context) {
+        context.getSharedPreferences(PREF_LOGIN_DETAILS, Context.MODE_PRIVATE)
+                .edit()
+                .remove(ACCOUNT_NAME)
+                .remove(ACCOUNT_AUTH_KEY)
+                .apply();
+    }
+
+    private static String createHint(String accountName) {
+        int idx = accountName.indexOf("@");
+        String namePart = accountName;
+        String rem = "";
+        if (idx > -1) {
+            namePart = accountName.substring(0, idx);
+            rem = accountName.substring(idx);
+        }
+
+        return namePart.substring(0, 2) + "****" + rem;
+    }
+
+    @Nullable
+    static String getLoginAccount(@NonNull Context context) {
+        return context.getSharedPreferences(PREF_LOGIN_DETAILS, Context.MODE_PRIVATE)
+                .getString(ACCOUNT_NAME, null);
+    }
+
+    static String getLoginAuthKey(Context context) {
+        return context.getSharedPreferences(PREF_LOGIN_DETAILS, Context.MODE_PRIVATE)
+                .getString(ACCOUNT_AUTH_KEY, null);
+    }
+
+    static String getLoginHint(Context context) {
+        return context.getSharedPreferences(PREF_APP_INFO, Context.MODE_PRIVATE)
+                .getString(ACCOUNT_NAME_HINT, null);
+    }
+
+    static Spanned getDebugText(@NonNull Context context) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("<b>SHARED PREFS DEBUG:<br></b>");
+        sb.append(ACCOUNT_NAME_HINT);
+        sb.append(": <i>");
+        sb.append(PrefUtils.getLoginHint(context));
+        sb.append("</i><br>\n");
+
+        sb.append(ACCOUNT_NAME);
+        sb.append(": <i>");
+        sb.append(PrefUtils.getLoginAccount(context));
+        sb.append("</i><br>\n");
+
+        sb.append(ACCOUNT_AUTH_KEY);
+        sb.append(": <i>");
+        sb.append(PrefUtils.getLoginAuthKey(context));
+        sb.append("</i><br>\n");
+        return Html.fromHtml(sb.toString());
+    }
+}
